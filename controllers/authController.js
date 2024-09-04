f237e0f28639a8989db825f2ddba5d66ccfad676
SkrypaD <skrypchenkodanil@gmail.com>
2024-09-04 16:24:42 +0000

    authController changed
    Implemented logic for register and login route

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,17 +4,21 @@ const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt')
 
 
-async function loginUserController(req, res, next){
+async function loginUserController(req, res){
     const { username, password } = req.body
 
+    if(!username || !password){
+        res.status(400).send({
+            success : false,
+            message : 'Username and password required!'
+        })
+    }
 
     try{
-        const query = 'SELECT * FROM users WHERE username = $1'
+        const query = 'SELECT * FROM users WHERE name = $1'
         const user = await pool.query(query, [username])
 
-        const hashedPassword = bcrypt.hash(password, process.env.SECRET)
-        const isMatch = bcrypt.compare(hashedPassword, user.rows[0].password)
-
+        const isMatch = await bcrypt.compare(password, user.rows[0].password)
         if(isMatch){
             let token = jwt.sign({ username : username }, process.env.SECRET,{
                 expiresIn : '1h'
@@ -23,13 +27,16 @@ async function loginUserController(req, res, next){
             res.cookie('token', token, {
                 httpOnly : true
             })
+            res.status(200).send({
+                success : true,
+                message : 'Logged in!!!'
+            })
         }else{
             res.status(401).send({
                 success : false,
                 message : "Invalid password"
             })
         }
-        next()
     }catch(err){
         res.status(400).send({
             success : false,
@@ -38,9 +45,34 @@ async function loginUserController(req, res, next){
     }
 }
 
-async function registerUserController(req, res, next){
-}
+async function registerUserController(req, res ){
+    const { username, password, email } = req.body
+    
+    if(!username || !password || !email){
+        return res.status(400).send({
+            status : false,
+            message : 'Invalid data'
+        })
+    }
 
+    try{
+        const query = 'INSERT INTO users VALUES ($1, $2, $3);'
+        const hashedPassword = await bcrypt.hash(password, (process.env.SALT_ROUNDS, 10))
+        const result = await pool.query(query, [username, hashedPassword, email])
+
+        res.status(200).send({
+            success : true,
+            message : 'Account registered'
+        })
+       
+    }catch(err){
+        console.log(err)
+        res.status(400).send({
+            success : false,
+            message : 'Could not create an account try again...',
+        })
+    }
+}
 
 module.exports = {
     loginUserController,
